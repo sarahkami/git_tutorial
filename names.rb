4903012ce961aed19e3b2ae1b66870aea4a0a271
SarahGanter <Sarah@Sarahs-MacBook-Air.local>
2015-07-07 18:38:06 +0200

    changed Nynne

--- names.rb
+++ names.rb
@@ -1,5 +1,5 @@
 names = Array.new
 
-names = ["nynne", "klaus", "erik", "sarah", "franzi", "laurie", "thea", "julius"]
+names = ["Nynnne", "klaus", "erik", "sarah", "franzi", "laurie", "thea", "julius"]
 
 puts names
